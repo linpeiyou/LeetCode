93fc19c45993d46e18290adbc235a3600664ed00
peige <1263292336@qq.com>
2017-07-05 08:47:55 +0800

    No.1 修改了解决办法

--- src/algorithms/TwoSum.java
+++ src/algorithms/TwoSum.java
@@ -1,6 +1,8 @@
 package algorithms;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * No.1 题目：
@@ -13,6 +15,8 @@ import java.util.Arrays;
 public class TwoSum {
 
 	public int[] twoSum(int[] nums, int target) {
+		// 之前的解决方法：
+		/*
 		for(int i = 0; i < nums.length; ++i) {
 			for(int j = i+1; j < nums.length; ++j) {
 				if(nums[i] + nums[j] == target) {
@@ -21,11 +25,27 @@ public class TwoSum {
 			}
 		}
 		return null;
+		*/
+		
+		// 使用Map，时间复杂度为O(n)
+		Map<Integer, Integer> map = new HashMap<>();
+		int result[] = new int[2];
+		for(int i = 0; i < nums.length; ++i) {
+			if(map.containsKey(target - nums[i])) {
+				int index = map.get(target - nums[i]);
+				result[0] = index;
+				result[1] = i;
+			}
+			map.put(nums[i], i);
+		}
+		return result;
 	}
 	
 	public static void main(String[] args) {
 		TwoSum t = new TwoSum();
 		int[] ints = t.twoSum(new int[] { 3, 2, 4 }, 6);
+		int[] ints2 = t.twoSum(new int[] { 2,7,11,15 }, 9);
 		System.out.println(Arrays.toString(ints));
+		System.out.println(Arrays.toString(ints2));
 	}
 }
